8e4ca5dcd6c310a1a76a93f8c6c19a253f0cabec
Eugene <eugene.popolamov@effective-soft.com>
2022-08-16 12:58:09 +0300

    feat: add args helper

--- weather.js
+++ weather.js
@@ -3,7 +3,16 @@ import getArgs from './helpers/args.js';
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(process.argv);
+  console.log(args);
+  if (args.h) {
+    // show help
+  }
+  if (args.s) {
+    // save city
+  }
+  if (args.t) {
+    // save token
+  }
 };
 
 initCLI();
